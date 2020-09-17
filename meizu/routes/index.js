177eee234ed02510fe3137f4d7a5876932e64221
lee-duke <416208744@qq.com>
2020-09-17 17:57:30 +0800

    商品详细信息图片数据库和接口更新

--- meizu/routes/index.js
+++ meizu/routes/index.js
@@ -4,65 +4,65 @@ const bodyParser = require('body-parser');
 var router = express.Router();
 
 // 配置跨区请求
-router.all('*', function(req, res ,next) {
+router.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', '*');
   next();
 });
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 // 首页
-router.get('/index', function(req, res, next) {
+router.get('/index', function (req, res, next) {
   // var handset, music, parts, life, hot, flyme, video
   var obj = {
-    "handset": [],"music": [], "parts": [], "life": [], "hot": [], "flyme": [], "video": []
+    "handset": [], "music": [], "parts": [], "life": [], "hot": [], "flyme": [], "video": []
   }
   var sql = `select * from products where type='handset'`;
-  db.query(sql, function(err, data) {
-    if(err) {
+  db.query(sql, function (err, data) {
+    if (err) {
       console.log(err)
-    }else {
+    } else {
       obj.handset = data;
       sql = `select * from products where type='music'`;
-      db.query(sql, function(err, data) {
-        if(err) {
+      db.query(sql, function (err, data) {
+        if (err) {
           console.log(err)
-        }else {
+        } else {
           obj.music = data;
           sql = `select * from products where type='parts'`;
-          db.query(sql, function(err, data) {
-            if(err) {
+          db.query(sql, function (err, data) {
+            if (err) {
               console.log(err)
-            }else {
+            } else {
               obj.parts = data;
               sql = `select * from products where type='life'`;
-              db.query(sql, function(err, data) {
-                if(err) {
+              db.query(sql, function (err, data) {
+                if (err) {
                   console.log(err)
-                }else {
+                } else {
                   obj.life = data;
                   sql = `select * from hot`;
-                  db.query(sql, function(err, data) {
-                    if(err) {
+                  db.query(sql, function (err, data) {
+                    if (err) {
                       console.log(err)
-                    }else {
+                    } else {
                       obj.hot = data;
                       sql = `select * from flyme`;
-                      db.query(sql, function(err, data) {
-                        if(err) {
+                      db.query(sql, function (err, data) {
+                        if (err) {
                           console.log(err)
-                        }else {
+                        } else {
                           obj.flyme = data;
                           sql = `select * from video`;
-                          db.query(sql, function(err, data) {
-                            if(err) {
+                          db.query(sql, function (err, data) {
+                            if (err) {
                               console.log(err)
-                            }else {
-                              obj.video =  data;
+                            } else {
+                              obj.video = data;
                               res.send(obj);
                             }
                           })
@@ -81,18 +81,18 @@ router.get('/index', function(req, res, next) {
 });
 
 // 论坛
-router.get('/forum', function(req, res, next) {
+router.get('/forum', function (req, res, next) {
   var essaySql = `select * from essay`;
-  db.query(essaySql, function(err, data) {
-    if(err) {
+  db.query(essaySql, function (err, data) {
+    if (err) {
       console.log(err)
-    }else {
+    } else {
       var essay = data;
       var invitationsSql = `select * from invitations`;
-      db.query(invitationsSql, function(err, data) {
-        if(err) {
+      db.query(invitationsSql, function (err, data) {
+        if (err) {
           console.log(err)
-        }else {
+        } else {
           var invitations = data;
         }
         res.send({
@@ -102,26 +102,26 @@ router.get('/forum', function(req, res, next) {
           msg: '请求成功'
         })
       });
-      
+
     }
   });
-  
+
 });
 
 // 全部商品
-router.get('/products', function(req, res, next) {
+router.get('/products', function (req, res, next) {
   console.log(req.query)
   var sql = `select * from products where type='${req.query.type}'`;
   db.query(sql, (err, data) => {
-    if(err) {
+    if (err) {
       console.log(err);
-    }else {
+    } else {
       var reqdata = data;
-      var sql1= `select * from products where type='commend'`;
+      var sql1 = `select * from products where type='commend'`;
       db.query(sql1, (err, data) => {
-        if(err) {
+        if (err) {
           console.log(err);
-        }else {
+        } else {
           res.send({
             code: 200,
             data: reqdata,
@@ -135,26 +135,38 @@ router.get('/products', function(req, res, next) {
 });
 
 // 单个商品
-router.get('/product/:id', function(req, res, next) {
+router.get('/product/:id', function (req, res, next) {
   // console.log(req.params.id)
   var sql = `select * from products where id=${req.params.id}`;
-  db.query(sql, function(err, data) {
-    if(err) {
+  db.query(sql, function (err, data) {
+    if (err) {
       console.log(err)
-    }else {
+    } else {
       // console.log(data);
       res.send(data)
     }
   });
 })
+// 商品详情图片
+router.get('/details', function (req, res, next) {
+  console.log(req.query)
+  var sql = `select * from details where model="${req.query.model}" && color="${req.query.color}"`
+  db.query(sql, function (err, data) {
+    if (err) {
+      console.log(err)
+    } else {
+      res.send(data[0])
+    }
+  })
+})
 
 // 综合论坛
-router.get('/discuss', function(req, res, next) {
+router.get('/discuss', function (req, res, next) {
   var sql = `select * from discuss`;
-  db.query(sql, function(err, data) {
-    if(err) {
+  db.query(sql, function (err, data) {
+    if (err) {
       console.log(err)
-    }else {
+    } else {
       // console.log(data);
       res.send(data)
     }
@@ -162,50 +174,50 @@ router.get('/discuss', function(req, res, next) {
 });
 
 // 专卖店
-router.get('/address', function(req, res, next) {
+router.get('/address', function (req, res, next) {
   var sql = `select * from province`;
-  db.query(sql, function(err, data) {
-    if(err){
+  db.query(sql, function (err, data) {
+    if (err) {
       console.log(err)
-    }else {
+    } else {
       res.send(data)
     }
   })
 });
-router.get('/city', function(req, res, next) {
+router.get('/city', function (req, res, next) {
   // console.log(req.query)
   var sql = `select * from city where provinceRid='${req.query.provinceRid}'`;
-  db.query(sql, function(err, data) {
-    if(err) {
+  db.query(sql, function (err, data) {
+    if (err) {
       console.log(err)
-    }else {
+    } else {
       res.send(data)
     }
   })
 });
-router.get('/store', function(req, res, next) {
+router.get('/store', function (req, res, next) {
   // console.log(req.query.cityRid)
   var sql = `select * from store where cityRid='${req.query.cityRid}'`;
-  db.query(sql, function(err, data) {
-    if(err) {
+  db.query(sql, function (err, data) {
+    if (err) {
       console.log(err)
-    }else {
+    } else {
       res.send(data)
     }
   })
 });
 
 // 社区影像馆
-router.get('/gallery', function(req, res, next) {
-  console.log(req.query.page)
+router.get('/gallery', function (req, res, next) {
+  // console.log(req.query.page)
   var sql = `select * from gallery`;
-  db.query(sql, function(err, data) {
-    if(err) {
+  db.query(sql, function (err, data) {
+    if (err) {
       console.log(err)
-    }else {
+    } else {
       // console.log(data)
       var galleryArr = []
-      for(let i = (req.query.page - 1) * 12; i < req.query.page * 12; i++) {
+      for (let i = (req.query.page - 1) * 12; i < req.query.page * 12; i++) {
         galleryArr.push(data[i])
       }
       res.send(galleryArr)
@@ -214,12 +226,12 @@ router.get('/gallery', function(req, res, next) {
 })
 
 // flyme系统更新
-router.get('/flyme/update', function(req, res, next) {
+router.get('/flyme/update', function (req, res, next) {
   var sql = `select * from phone`
-  db.query(sql, function(err, data) {
-    if(err) {
+  db.query(sql, function (err, data) {
+    if (err) {
       console.log(err)
-    }else {
+    } else {
       // console.log(data)
       res.send(data)
     }
@@ -227,20 +239,19 @@ router.get('/flyme/update', function(req, res, next) {
 })
 
 // 插入数据
-router.get('/intodata', function(req, res, next) {
+router.get('/intodata', function (req, res, next) {
+
+  console.log(req.query)
+  // console.log(str)
+  var sql = `insert into details (small_img, big_img) values (${req.query.small},${req.query.big})`
+  db.query(sql, function(err,data) {
+    if(err){
+      console.log(err)
+    }else{
+       res.send('OK')
+    }
+  })
 
 
-  // var obj = req.query.obj
-  // for(let item in obj) {
-  //   var sql = `insert into store values ('${obj[item].name}', '${obj[item].address}', '${obj[item].addRemark}', '${obj[item].tel}', '${obj[item].cityId}')`;
-  //   db.query(sql, function(err, data) {
-  //     if(err) {
-  //       console.log(err)
-  //     }else {
-  //       // console.log(data)
-  //     }
-  //   });
-  // }
-  
 });
 module.exports = router;
